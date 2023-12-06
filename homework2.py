02dab1c3a849fd7c383b48a4f6fb6bb1a1229d90
Allmarr1 <mabelozerov@gmail.com>
2023-12-06 11:31:34 +0200

    created homework

--- homework2.py
+++ homework2.py
@@ -1,1 +1,7 @@
-print (""hello)
+UAN_USD = 37.55
+amount_uah = int(input("print amount of uah"))
+
+result = amount_uah // UAN_USD
+print(f"Ваша сума доларів {result}")
+
+# print(f" {*>6} {1000:*>4} {1:*>4}")
