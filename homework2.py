07e5d4508ea57a6c7a657aa1e5f90e09a0eb74f1
Allmarr1 <mabelozerov@gmail.com>
2023-12-07 16:05:31 +0200

    start

--- homework2.py
+++ homework2.py
@@ -1,7 +1,17 @@
 UAN_USD = 37.55
-amount_uah = int(input("print amount of uah"))
+UAN_EUR = 40.50
+UAN_PLN = 9.45
+uah = int(input("print amount of uah"  ))
 
-result = amount_uah // UAN_USD
-print(f"Ваша сума доларів {result}")
+buy1 = uah // UAN_USD
+buy2 = uah // UAN_EUR
+buy3 = uah // UAN_PLN
+sell1 = buy1 + buy1 * 0.05
+sell2 = buy2 + buy2 * 0.05
+sell3 = buy3 + buy3 * 0.05
+print(f"{:*>15}")
+print(f"*{buy1:*>5}" "USD" f"{sell1:*<5}*")
+print(f"*{buy2:*>5}" "USD" f"{sell2:*<5}*")
+print(f"*{buy3:*>5}" "USD" f"{sell3:*<5}*")
 
-# print(f" {*>6} {1000:*>4} {1:*>4}")
+print(f" {1:*>4}")
