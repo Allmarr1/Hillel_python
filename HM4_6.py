4151860a69281255fe5209763cd35d59b1c66d41
Allmarr1 <mabelozerov@gmail.com>
2023-12-17 11:07:27 +0200

    HM4_6 done

--- HM4_6.py
+++ HM4_6.py
@@ -1,11 +1,15 @@
-n = 7
+u_input = input("Введіть число від 6 до 16: ")
+if u_input.isdigit():
+    n = int(u_input)
 
+    if 6<= n <= 16:
+        for i in range(n - 4, n + 4):
+            for j in range(2, 10):
+                res = str(i*j)
+                print(j, '*', str(i).rjust(2), '=', res.rjust(3), end='   ')
+            print()
+    else:
+        print("Ви ввели число поза межами заданого діапазону")
 
-for i in range(n - 4, n + 4):
-    for j in range(2, 10):
-        res = str(i*j)
-        print(j, '*', str(i).rjust(2), '=', res.rjust(3), end='   ')
-    print()
-
-# a=5
-# b=100
+else:
+    print("Введіть ціле число")
