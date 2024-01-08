aa5e631da845c0bebee97a5cf0d376775717ed73
Allmarr1 <mabelozerov@gmail.com>
2024-01-08 18:09:10 +0200

    created HM5_2.py

--- homework5/HM5_2.py
+++ homework5/HM5_2.py
@@ -1,8 +1,19 @@
-# array = [1, 1, 2, 2, 3]
+def second_smallest(a):
+    min_val = a[0]
+    for i in range(len(a)-1):
+        sec_val = a[i+1]
+        if min_val != sec_val:
+            break
+    if not min_val < sec_val:
+        min_val, sec_val = sec_val, min_val
+    for i in range(len(a)-2):
+        x = a[i+2]
+        if x < min_val:
+            min_val, sec_val = x, min_val
+        elif x < sec_val and not x == min_val:
+            sec_val = x
 
-def second_smallest(array):
-    array.sort()
-    return array[1]
+    return sec_val
 
 assert second_smallest([1, 1, 2, 2, 3]) == 2
 assert second_smallest([-1, 10, -2, 2]) == -1
