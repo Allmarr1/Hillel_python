0b5b350bf8395805d6869f7d649d789db3375bfc
Allmarr1 <mabelozerov@gmail.com>
2024-01-17 17:36:13 +0200

    created hm7

---
+++ homework7/hm7_2.py
@@ -0,0 +1,25 @@
+def check_parenthes(string):
+    stack = []
+    for char in string:
+        if char == ("(", "[", "{"):
+            stack.append(char)
+        else:
+            try:
+                if stack[-1] == ("(", "[", "{"):
+                    stack.pop()
+            except IndexError:
+                stack.append(char)
+    if len(stack) > 0:
+        return False
+    return True
+
+
+
+assert check_parenthes("(())") == True
+assert check_parenthes(")(") == False
+assert check_parenthes("()(") == False
+assert check_parenthes(")())") == False
+assert check_parenthes("")
+assert check_parenthes("asasa(dsdsd)(") == False
+assert check_parenthes("skaslka[{ddfdfsdd}()]") ==True
+assert check_parenthes("}{kdlsds(p[])") == False
---
+++ homework7/hm7_3.py
@@ -0,0 +1,11 @@
+def expanded_list(nested_list):
+    list1 = []
+    for i in nested_list:
+        if isinstance(i, list):
+            list1.extend(expanded_list(i))
+        else:
+            list1.append(i)
+    print(list1)
+    return list1
+
+assert expanded_list([1, 2, 3, 'hello', [7, 8, [5, 7], 9], 'world', [77, 88, 99]]) == [1, 2, 3, 'hello', 7, 8, 5,7, 9, 'world', 77, 88, 99]
